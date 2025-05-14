4f99efd27c2a4e91f21213afb18a1e360fd5f1e0
CoveTar <covetar@samale.ca>
2025-05-14 01:18:47 -0400

    nvim

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,24 +1,22 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
-end
-vim.opt.rtp:prepend(lazypath)
+vim.opt.rtp:prepend(vim.fn.stdpath("data") .. "/lazy/lazy.nvim")
 
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    {
+      "LazyVim/LazyVim",
+      import = "lazyvim.plugins",
+      opts = {
+        colorscheme = function()
+          require("tinted-colorscheme").setup(nil, {})
+        end,
+      },
+    },
     -- import/override with your plugins
+    {
+      "folke/tokyonight.nvim",
+      opts = { style = "night" },
+    },
     { import = "plugins" },
   },
   defaults = {
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,21 +1,9 @@
 return {
-
-  -- tokyonight
-  {
-    "folke/tokyonight.nvim",
-    lazy = true,
-    opts = { style = "night" },
-  },
   -- Add base16-nvim plugin
-  {
-    "RRethy/base16-nvim",
-    lazy = true, -- Lazy load the plugin
-  },
+  -- {
+  --   "RRethy/base16-nvim",
+  --   lazy = true, -- Lazy load the plugin
+  -- },
+  { "tinted-theming/tinted-nvim" },
   -- Configure LazyVim to use the equilibrium-dark colorscheme
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "base16-equilibrium-dark",
-    },
-  },
 }
